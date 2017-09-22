c996d30131cc3b988504e09a80422423d29937d6
hwang595 <hongyiwang@cs.wisc.edu>
2017-09-21 20:46:42 -0500

    edit run script

--- Pytorch_code/tools/run_pytorch.sh
+++ Pytorch_code/tools/run_pytorch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+mkdir ../cifar10_on_resnet/output/
+
 python ../cifar10_on_resnet/cifar10_on_resnet.py --lr=0.1 \
 --epochs=450 \
 --momentum=0.9 \
 --test-batch-size=1000 \
---enable-gpu=True\
+--enable-gpu=True >> ../cifar10_on_resnet/output/out\
 
 
